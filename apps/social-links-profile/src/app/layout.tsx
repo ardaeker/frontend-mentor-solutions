c0b19a136e0369bce58aaeaafbfbc49357297cde
Arda Eker <ardaekereu@gmail.com>
2024-06-28 20:15:10 +0300

    fix: update social links profile page URL in metadata

--- apps/social-links-profile/src/app/layout.tsx
+++ apps/social-links-profile/src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   },
   keywords: ["nextjs", "tailwindcss", "frontend-mentor", "challenge"],
   openGraph: {
-    url: "#",
+    url: "https://fm-social-links-profile.vercel.app/",
     type: "profile",
     title: "Jessica Randall",
     description: "Front-end developer and avid reader.",
